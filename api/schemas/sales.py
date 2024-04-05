1eb0c6b44e6f929337a016c09cabf96fcec6c2a0
Akihito Niko Seki <akihitonikoseki@Akihitos-MacBook-Pro.local>
2024-04-05 20:34:12 +0900

    fix sales schema

--- api/schemas/sales.py
+++ api/schemas/sales.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel, Field, condecimal
 class SaleBase(BaseModel):
     name: str
     amount: Optional[int] = Field(default=1)
-    price: Optional[float] = Field(gt=0)
+    price: Optional[float] = 0
 
 class SaleCheck(BaseModel):
     sales: float = Field(gt=0)
