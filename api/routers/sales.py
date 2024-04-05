63a34bf9b5a42ed35ef218a8a12da438950f1c26
Akihito Niko Seki <akihitonikoseki@Akihitos-MacBook-Pro.local>
2024-04-05 20:15:11 +0900

    fix: router paths

--- api/routers/sales.py
+++ api/routers/sales.py
@@ -10,13 +10,13 @@ router = APIRouter(
     prefix="/v1/sales"
 )
 
-@router.post("/", response_model=sales_schema.SaleBase)
+@router.post("", response_model=sales_schema.SaleBase)
 async def create_sale(
     sale_body: sales_schema.SaleBase, db: AsyncSession = Depends(get_db)
 ):
     return await sales_crud.create_sale(db, sale_body)
 
-@router.get("/", response_model=sales_schema.SaleCheck)
+@router.get("", response_model=sales_schema.SaleCheck)
 async def check_sale(
     db: AsyncSession = Depends(get_db)
 ):
